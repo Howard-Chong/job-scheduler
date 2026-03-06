9fe6ef061b5f993cb6bb43cb76f287f6af980483
Howard Chong <howardchong57@gmail.com>
2026-03-06 17:24:00 +0800

    chores: change the congiguration in jobscheduler

--- src/main/java/com/howard/job/scheduler/JobSchedulerApplication.java
+++ src/main/java/com/howard/job/scheduler/JobSchedulerApplication.java
@@ -3,9 +3,7 @@ package com.howard.job.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class JobSchedulerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobSchedulerApplication.class, args);
