ceb7c69659e1d13e74529456b69aa42fea7f7b54
Howard Chong <howardchong57@gmail.com>
2026-03-06 17:11:19 +0800

    disable security auto configuration for developement

--- src/main/java/com/howard/job/scheduler/JobSchedulerApplication.java
+++ src/main/java/com/howard/job/scheduler/JobSchedulerApplication.java
@@ -3,11 +3,11 @@ package com.howard.job.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class JobSchedulerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JobSchedulerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JobSchedulerApplication.class, args);
+    }
 }
